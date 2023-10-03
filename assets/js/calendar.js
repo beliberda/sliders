334426aff5e0a50f57e66ce3e3d2660006fe5214
beliberda <anton.nurgaliev@mail.ru>
2023-10-03 15:04:16 +1000

    arrows made

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,84 +1,70 @@
-
-
 function createCalendar(elem, year, month) {
-  elem = document.querySelector(elem)
-  let mon = month - 1
+  elem = document.querySelector(elem);
+  let calendarMonthYear = document.querySelector(".calendar-month-year");
+  let mon = month - 1;
   const date = new Date(year, mon);
-  const monthName = date.toLocaleString('default', { month: 'long' });
-  let d = new Date(year, mon)
+  const monthName = date.toLocaleString("default", { month: "long" });
+  let d = new Date(year, mon);
+
+  calendarMonthYear.innerHTML = `${monthName} ${year}`;
+
   let table = `
-    <div class="calendar-header">
-        <span class="calendar-left-button" id="prev"> &#9668;</span>
-
-        <span class="calendar-month-year" id="label">${monthName} ${year}</span>
-
-        <span class="calendar-right-button" id="next"> &#9658;</span>
-      </div>
-      <table id="days">
-        <tbody>
-          <td>Пн</td>
-          <td>Вт</td>
-          <td>Ср</td>
-          <td>Чт</td>
-          <td>Пт</td>
-          <td>Сб</td>
-          <td>Вс</td>
-        </tbody>
-      </table>
       <div id="calendar-frame">
         <table class="curr">
           <tbody>
             <tr>
           
-    `
+    `;
 
   for (let i = 0; i < getDay(d); i++) {
-    table += '<td></td>'
+    table += "<td></td>";
   }
 
   while (d.getMonth() == mon) {
-    table += '<td>' + d.getDate() + '</td>'
+    table += "<td>" + d.getDate() + "</td>";
     if (getDay(d) % 7 == 6) {
-      table += '</tr><tr>'
+      table += "</tr><tr>";
     }
-    d.setDate(d.getDate() + 1)
+    d.setDate(d.getDate() + 1);
   }
   if (getDay(d) != 0) {
     for (let i = getDay(d); i < 7; i++) {
-      table += '<td></td>'
+      table += "<td></td>";
     }
   }
 
   table += `</tr></tbody>
         </table>
-      </div>`
+      </div>`;
 
-  elem.innerHTML = table
+  elem.innerHTML = table;
 }
 
 function getDay(date) {
   let day = date.getDay();
   if (day == 0) day = 7;
-  return day - 1
+  return day - 1;
 }
 
-
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-
-
-});
-
-let prev = document.querySelector('#prev')
-let next = document.querySelector('#next')
-console.log(prev);
+document.addEventListener("DOMContentLoaded", () => {});
+let prev = document.querySelector("#prev");
+let next = document.querySelector("#next");
 let year = new Date().getFullYear();
-let month = new Date().getMonth()
-createCalendar('#calendar', year, month + 1)
-prev.addEventListener('click', () => {
-  console.log('dwqdw');
-  month = month - 1
-  createCalendar('#calendar', year, month + 1)
-})
+let month = new Date().getMonth();
+createCalendar("#calendar-frame", year, month + 1);
+prev.addEventListener("click", () => {
+  month--;
+  if (month == -1) {
+    year--;
+    month = 11;
+  }
+  createCalendar("#calendar-frame", year, month + 1);
+});
+next.addEventListener("click", () => {
+  month++;
+  if (month == 12) {
+    year++;
+    month = 0;
+  }
+  createCalendar("#calendar-frame", year, month + 1);
+});
