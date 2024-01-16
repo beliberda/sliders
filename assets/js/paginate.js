10bd6a3c14658e3d0ae7ae1ce5e2a085c2603693
beliberda <anton.nurgaliev@mail.ru>
2024-01-16 15:16:25 +1000

    pag btn

--- assets/js/paginate.js
+++ assets/js/paginate.js
@@ -1,15 +1,37 @@
+let list = document.querySelector(".post-List")
+let prev = document.querySelector("#prev")
+let next = document.querySelector("#next")
+const fetchA = async (page) => {
 
-const fetchA = async () => {
 
-    let list = document.querySelector("")
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let response = await fetch(`https://jsonplaceholder.typicode.com/posts/?_page=${page}&_limit=5`)
 
     let mass = await response.json()
     console.log(mass);
 
 
-
+    list.innerHTML = ""
+    mass.forEach(element => {
+        list.insertAdjacentHTML('afterbegin', `<div class="post-item">
+        <h2>${element.title}</h2>
+        <p class="text">${element.body}</p>
+      </div>`)
+    });
 
 }
 
-fetchA()
+let page = 1
+fetchA(page)
+
+prev.onclick = () => {
+    if (page > 1) {
+        page--
+    }
+    fetchA(page)
+}
+next.onclick = () => {
+    if (page > 1) {
+        page++
+    }
+    fetchA(page)
+}
